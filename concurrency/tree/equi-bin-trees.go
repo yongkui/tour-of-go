6e1a67362329e13d56b94ecadad6246e86d16c80
yongkui <yongkui_wang@163.com>
2021-02-05 09:59:24 +0800

    added tree

---
+++ concurrency/tree/equi-bin-trees.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	walkTree(t, ch)
+	close(ch)
+}
+
+func walkTree(t *tree.Tree, ch chan int) {
+	if t != nil {
+		walkTree(t.Left, ch)
+		ch <- t.Value
+		walkTree(t.Right, ch)
+	}
+
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	c1 := make(chan int)
+	c2 := make(chan int)
+
+	go Walk(t1, c1)
+	go Walk(t2, c2)
+
+	for i := range c1 {
+		if i != <-c2 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
+
+	for i := range ch {
+		fmt.Println(i)
+	}
+
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+
+}
